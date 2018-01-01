614fe4f264427587a39f763f71fc65bf4bd6108e
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-01 19:17:08 +0000

    Created getBalance() function in account.js file.
    All test green.

--- src/account.js
+++ src/account.js
@@ -3,6 +3,7 @@
   function Account(name, surname) {
     this.name = name
     this.surname = surname
+    this.balance = 0
   }
 
   Account.prototype.getName = function () {
@@ -11,6 +12,10 @@
 
   Account.prototype.getSurname = function () {
     return this.surname
+  };
+
+  Account.prototype.getBalance = function () {
+    return this.balance
   };
 
   exports.Account = Account;
