27276463128634414a4bbd02a1ba9e5d62d6d788
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-01 19:07:43 +0000

    Created getName and getSuname function in the account.js file.
    This two method simply get the information name and surname from the variable.

--- src/account.js
+++ src/account.js
@@ -5,7 +5,13 @@
     this.surname = surname
   }
 
+  Account.prototype.getName = function () {
+    return this.name
+  };
 
+  Account.prototype.getSurname = function () {
+    return this.surname
+  };
 
   exports.Account = Account;
 })(this);
