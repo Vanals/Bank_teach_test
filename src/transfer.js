55a9eb53c3acfcb3558f0f6e15e1d5f7783c17b9
Marco Vanali <Marco91Vanali@gmail.com>
2018-01-02 16:59:22 +0000

    Installed ESLint

--- src/transfer.js
+++ src/transfer.js
@@ -1,37 +1,37 @@
 (function(exports) {
 
   function Transfer() {
-    this.balanceAfterTransfer = 0
-    this.moneyTransfered = 0
-    this.date = "NotGiven"
+    this.balanceAfterTransfer = 0;
+    this.moneyTransfered = 0;
+    this.date = 'NotGiven';
   }
 
-  Transfer.prototype.recordTransfer = function (moneyTransfering, Balance) {
-    this.balanceAfterTransfer = Balance
-    this.moneyTransfered = moneyTransfering
-    this._recordDate()
+  Transfer.prototype.recordTransfer = function(moneyTransfering, Balance) {
+    this.balanceAfterTransfer = Balance;
+    this.moneyTransfered = moneyTransfering;
+    this._recordDate();
   };
 
-  Transfer.prototype._recordDate = function () {
+  Transfer.prototype._recordDate = function() {
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+    var mm = today.getMonth()+1; // January is 0!
     var yyyy = today.getFullYear();
 
-    if(dd<10) {
-        dd = '0'+dd
+    if (dd<10) {
+        dd = '0'+dd;
     }
 
-    if(mm<10) {
-        mm = '0'+mm
+    if (mm<10) {
+        mm = '0'+mm;
     }
 
     today = mm + '/' + dd + '/' + yyyy;
-    return this.date = today
+    return this.date = today;
   };
 
-  Transfer.prototype._getDate = function () {
-    return this.date
+  Transfer.prototype._getDate = function() {
+    return this.date;
   };
 
 
